b3fb99d6444d94693df8381fc352bb3e8e9a4488
palind1990 <justin.ooghe@gmail.com>
2021-06-17 18:47:02 +0200

    adding chat part1

--- _public/scripts/status.js
+++ _public/scripts/status.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   if (!navigator.onLine) {
     const statusElem = document.querySelector('.page-status')
-    statusElem.innerHTML = 'offline'
+    statusElem.innerHTML = 'offline';
   }
 }
--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 const express = require('express')
+const socketIO = require("socket.io");
+const app = express();
+const server = app.listen(9000);
+const io = socketIO(server);
 const path = require('path')
 
-const httpPort = 80
+const httpPort = 8000
 
-const app = express()
+var users = {};
+var name = '';
 
 app.use(express.static(path.join(__dirname, '_public')))
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '_public/index.html'))
 })
+
+app.get('/:name', function(req, res){
+  name = req.params.name;
+  res.sendFile(path.join(__dirname, "/" + name + ".html"));
+});
+
+io.sockets.on("connection", function(socket){
+  users[socket.id] = name;
+  // node
+  socket.on("nRoom", function(room){
+      socket.join(room);
+      socket.broadcast.in(room).emit("node new user", users[socket.id] + " new user has joined");
+  });
+
+  socket.on("node new message", function(data){
+      io.sockets.in("nRoom").emit('node news', users[socket.id] + ": "+ data);
+  });
+
+  // python
+  socket.on("pRoom", function(room){
+      socket.join(room);
+      socket.broadcast.in(room).emit("python new user", users[socket.id] + " new user has joined");
+  });
+
+  socket.on("python new message", function(data){
+      io.sockets.in("pRoom").emit('python news', users[socket.id] + ": "+ data);
+  });
+});
 
 app.listen(httpPort, function () {
   console.log(`Listening on port ${httpPort}!`)
